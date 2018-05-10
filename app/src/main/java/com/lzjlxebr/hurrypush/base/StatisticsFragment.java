1bcd3dae42980b761f4eead0391dc1eb222d762e
lzjlxebr <tyyzmyd@163.com>
2018-05-10 22:59:00 +0800

    presents data to view,but not perfect,just ok

--- app/src/main/java/com/lzjlxebr/hurrypush/base/StatisticsFragment.java
+++ app/src/main/java/com/lzjlxebr/hurrypush/base/StatisticsFragment.java
@@ -1,112 +1,137 @@
 package com.lzjlxebr.hurrypush.base;
 
 
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.haibin.calendarview.Calendar;
 import com.haibin.calendarview.CalendarLayout;
 import com.haibin.calendarview.CalendarView;
 import com.lzjlxebr.hurrypush.R;
+import com.lzjlxebr.hurrypush.db.HurryPushContract;
+import com.lzjlxebr.hurrypush.util.DateFormatterUtils;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class StatisticsFragment extends Fragment implements CalendarView.OnDateSelectedListener,
         CalendarView.OnYearChangeListener,
-        View.OnClickListener{
-    TextView mTextMonthDay;
+        CalendarView.OnMonthChangeListener,
+        View.OnClickListener, LoaderManager.LoaderCallbacks<Cursor> {
 
-    TextView mTextYear;
+    public static final int INDEX_COLUMN_START_TIME = 0;
 
-    TextView mTextLunar;
-
-    TextView mTextCurrentDay;
-
-    CalendarView mCalendarView;
-
-    RelativeLayout mRelativeTool;
-    private int mYear;
     CalendarLayout mCalendarLayout;
+    public static final int INDEX_COLUMN_END_TIME = 1;
+    public static final int INDEX_COLUMN_IS_USER_FINISH = 2;
+    public static final int INDEX_COLUMN_GAIN_EXP = 3;
+    public static final int INDEX_COLUMN_SMELL = 4;
+    public static final int INDEX_COLUMN_CONSTIPATION = 5;
+    public static final int INDEX_COLUMN_STICKINESS = 6;
+    public static final int INDEX_COLUMN_OVERALL_RATING = 7;
+    public static final int INDEX_COLUMN_SERVER_CALL_BACK = 8;
+    private static final int LOADER_ID = 2002;
+    public static String[] DEFECATION_RECORD_PROJECTION = {
+            HurryPushContract.DefecationRecordEntry.COLUMN_START_TIME,
+            HurryPushContract.DefecationRecordEntry.COLUMN_END_TIME,
+            HurryPushContract.DefecationRecordEntry.COLUMN_IS_USER_FINISH,
+            HurryPushContract.DefecationRecordEntry.COLUMN_GAIN_EXP,
+            HurryPushContract.DefecationRecordEntry.COLUMN_SMELL,
+            HurryPushContract.DefecationRecordEntry.COLUMN_CONSTIPATION,
+            HurryPushContract.DefecationRecordEntry.COLUMN_STICKINESS,
+            HurryPushContract.DefecationRecordEntry.COLUMN_OVERALL_RATING,
+            HurryPushContract.DefecationRecordEntry.COLUMN_SERVER_CALL_BACK
+    };
+    CalendarView mCalendarView;
+    TextView mCurrentDate, mTvDefecationDesc;
+    Calendar mCalendar;
+    private Cursor mCursor;
+
 
     @Override
     public void onClick(View v) {
 
     }
 
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_statistics,container,false);
+        View view = inflater.inflate(R.layout.fragment_statistics, container, false);
+
+
         initView(view);
-        initData();
+        getLoaderManager().initLoader(LOADER_ID, null, this);
+
         return view;
     }
 
     private void initView(View view) {
 
-        mTextMonthDay = (TextView) view.findViewById(R.id.tv_month_day);
-        mTextYear = (TextView) view.findViewById(R.id.tv_year);
-        mTextLunar = (TextView) view.findViewById(R.id.tv_lunar);
-        mRelativeTool = (RelativeLayout) view.findViewById(R.id.rl_tool);
-        mCalendarView = (CalendarView) view.findViewById(R.id.calendarView);
-        mTextCurrentDay = (TextView) view.findViewById(R.id.tv_current_day);
-        mTextMonthDay.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!mCalendarLayout.isExpand()) {
-                    mCalendarView.showYearSelectLayout(mYear);
-                    return;
-                }
-                mCalendarView.showYearSelectLayout(mYear);
-                mTextLunar.setVisibility(View.GONE);
-                mTextYear.setVisibility(View.GONE);
-                mTextMonthDay.setText(String.valueOf(mYear));
-            }
-        });
-        view.findViewById(R.id.fl_current).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mCalendarView.scrollToCurrent();
-            }
-        });
-        mCalendarLayout = (CalendarLayout) view.findViewById(R.id.calendarLayout);
+        mCalendarView = view.findViewById(R.id.calendarView);
+        mCurrentDate = view.findViewById(R.id.tv_current_date_indicator);
+        mTvDefecationDesc = view.findViewById(R.id.tv_defecation_desc);
+
+        int year = mCalendarView.getCurYear();
+        int month = mCalendarView.getCurMonth();
+        int day = mCalendarView.getCurDay();
+        String indicator_text = year + "年" + month + "月" + day + "日";
+        mCurrentDate.setText(indicator_text);
+
+
+        mCalendarLayout = view.findViewById(R.id.calendarLayout);
         mCalendarView.setOnDateSelectedListener(this);
         mCalendarView.setOnYearChangeListener(this);
-        mTextYear.setText(String.valueOf(mCalendarView.getCurYear()));
-        mYear = mCalendarView.getCurYear();
-        mTextMonthDay.setText(mCalendarView.getCurMonth() + "月" + mCalendarView.getCurDay() + "日");
-        mTextLunar.setText("今日");
-        mTextCurrentDay.setText(String.valueOf(mCalendarView.getCurDay()));
+        mCalendarView.setOnMonthChangeListener(this);
     }
 
     private void initData() {
+        mCalendar = mCalendarView.getSelectedCalendar();
         List<Calendar> schemes = new ArrayList<>();
-        int year = mCalendarView.getCurYear();
-        int month = mCalendarView.getCurMonth();
 
-        schemes.add(getSchemeCalendar(year, month, 3, 0xFF40db25,"假"));
-        schemes.add(getSchemeCalendar(year, month, 6, 0xFFe69138,"事"));
-        schemes.add(getSchemeCalendar(year, month, 9, 0xFFdf1356,"议"));
-        schemes.add(getSchemeCalendar(year, month, 13, 0xFFedc56d,"记"));
-        schemes.add(getSchemeCalendar(year, month, 14, 0xFFedc56d,"记"));
-        schemes.add(getSchemeCalendar(year, month, 15, 0xFFaacc44,"假"));
-        schemes.add(getSchemeCalendar(year, month, 18, 0xFFbc13f0,"记"));
-        schemes.add(getSchemeCalendar(year, month, 25, 0xFF13acf0,"假"));
-        schemes.add(getSchemeCalendar(year, month, 27, 0xFF13acf0,"多"));
+        int count = mCursor.getCount();
+        int itemCount = mCursor.getColumnCount();
+
+        for (int i = 0; i < count; i++) {
+            mCursor.moveToPosition(i);
+
+            long startTime = mCursor.getLong(INDEX_COLUMN_START_TIME);
+            long endTime = mCursor.getLong(INDEX_COLUMN_END_TIME);
+            float gain_exp = mCursor.getFloat(INDEX_COLUMN_GAIN_EXP);
+            int smell = mCursor.getInt(INDEX_COLUMN_SMELL);
+            int constipation = mCursor.getInt(INDEX_COLUMN_CONSTIPATION);
+            int stickiness = mCursor.getInt(INDEX_COLUMN_STICKINESS);
+            float overall_rating = mCursor.getLong(INDEX_COLUMN_OVERALL_RATING);
+            int server_call_back = mCursor.getInt(INDEX_COLUMN_SERVER_CALL_BACK);
+
+            int year = DateFormatterUtils.getYearFromLong(startTime);
+            int month = DateFormatterUtils.getMonthFromLong(startTime);
+            int day = DateFormatterUtils.getDayFromLong(startTime);
+
+            //String smellStr = smell == 0 ? "" : "";
+
+            String text = "获得经验：" +
+                    gain_exp + "，闻起来：" + smell + "，通畅度：" + constipation + "，黏稠度：" +
+                    stickiness + "，综合评分：" + overall_rating + "。上传服务器状态：" + server_call_back;
+
+            schemes.add(getSchemeCalendar(year, month, day, 0xff3e3e3e, text));
+        }
         mCalendarView.setSchemeDate(schemes);
     }
 
 
-    private Calendar getSchemeCalendar(int year, int month, int day, int color,String text) {
+    private Calendar getSchemeCalendar(int year, int month, int day, int color, String text) {
         Calendar calendar = new Calendar();
         calendar.setYear(year);
         calendar.setMonth(month);
@@ -114,26 +139,81 @@ public class StatisticsFragment extends Fragment implements CalendarView.OnDateS
         calendar.setSchemeColor(color);//如果单独标记颜色、则会使用这个颜色
         calendar.setScheme(text);
         calendar.addScheme(new Calendar.Scheme());
-        calendar.addScheme(0xFF008800,"假");
-        calendar.addScheme(0xFF008800,"节");
+//        calendar.addScheme(0xFF008800, "假");
+//        calendar.addScheme(0xFF008800, "节");
         return calendar;
     }
 
     @Override
     public void onYearChange(int year) {
-        mTextMonthDay.setText(String.valueOf(year));
+
     }
 
     @Override
     public void onDateSelected(Calendar calendar, boolean isClick) {
+        mCalendar = calendar;
+        int year = calendar.getYear();
+        int month = calendar.getMonth();
+        int day = calendar.getDay();
+        String indicator_text = year + "年" + month + "月" + day + "日";
+        mCurrentDate.setText(indicator_text);
 
-        mTextLunar.setVisibility(View.VISIBLE);
-        mTextYear.setVisibility(View.VISIBLE);
-        mTextMonthDay.setText(calendar.getMonth() + "月" + calendar.getDay() + "日");
-        mTextYear.setText(String.valueOf(calendar.getYear()));
-        mTextLunar.setText(calendar.getLunar());
-        mYear = calendar.getYear();
+        String text = calendar.getScheme();
+
+        if (text == null || "".equals(text)) {
+            text = "当前所选日期暂无详情查看。";
+        }
+
+        mTvDefecationDesc.setText(text);
     }
 
 
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+        //long startOfDay = DateFormatterUtils.getLongDateValueFromIntValues(mCalendar.getYear(), mCalendar.getMonth(), mCalendar.getDay());
+        //long endOfDay = DateFormatterUtils.getEndOfMonthLongValueFromStart(startOfDay);
+
+        //String selection = "start_time between ? and ?";
+
+        //String sortOrder = "start_time";
+
+        switch (id) {
+            case LOADER_ID: {
+                Uri defecationRecordUri = HurryPushContract.DefecationRecordEntry.DEFECATION_RECORD_URI;
+                return new CursorLoader(
+                        getActivity(),
+                        defecationRecordUri,
+                        DEFECATION_RECORD_PROJECTION,
+                        null,
+                        null,
+                        null
+                );
+            }
+            default:
+                throw new RuntimeException("Loader Not Implemented" + id);
+        }
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
+        swapCursor(data);
+        initData();
+        //mCursor.close();
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+        swapCursor(null);
+    }
+
+    private void swapCursor(Cursor cursor) {
+        mCursor = cursor;
+    }
+
+
+    @Override
+    public void onMonthChange(int year, int month) {
+        getLoaderManager().getLoader(LOADER_ID).forceLoad();
+    }
 }
