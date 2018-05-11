0a1fc9c706a4520b50cbef16e663a364b277e807
lzjlxebr <tyyzmyd@163.com>
2018-05-11 21:49:14 +0800

    defecation record entry

---
+++ app/src/main/java/com/lzjlxebr/hurrypush/entity/DefecationFinalRecord.java
@@ -0,0 +1,49 @@
+package com.lzjlxebr.hurrypush.entity;
+
+public class DefecationFinalRecord {
+    private long id;
+    private int smell;
+    private int constipation;
+    private int stickiness;
+    private double overallRatting;
+    private double gainExp;
+    private int isUserFinished;
+
+    public DefecationFinalRecord(long id, int smell, int constipation, int stickiness, double overallRatting, double gainExp, int isUserFinished) {
+        this.id = id;
+        this.smell = smell;
+        this.constipation = constipation;
+        this.stickiness = stickiness;
+        this.overallRatting = overallRatting;
+        this.gainExp = gainExp;
+        this.isUserFinished = isUserFinished;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public int getSmell() {
+        return smell;
+    }
+
+    public int getConstipation() {
+        return constipation;
+    }
+
+    public int getStickiness() {
+        return stickiness;
+    }
+
+    public double getOverallRatting() {
+        return overallRatting;
+    }
+
+    public double getGainExp() {
+        return gainExp;
+    }
+
+    public int getIsUserFinished() {
+        return isUserFinished;
+    }
+}
