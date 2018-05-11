06698d012f44547c25dae132542f89d4c8c8b55d
lzjlxebr <tyyzmyd@163.com>
2018-05-12 01:37:04 +0800

    another event bus event

---
+++ app/src/main/java/com/lzjlxebr/hurrypush/entity/NetSyncTaskComplete.java
@@ -0,0 +1,9 @@
+package com.lzjlxebr.hurrypush.entity;
+
+public class NetSyncTaskComplete {
+    private boolean finish;
+
+    public NetSyncTaskComplete(boolean finish) {
+        this.finish = finish;
+    }
+}
