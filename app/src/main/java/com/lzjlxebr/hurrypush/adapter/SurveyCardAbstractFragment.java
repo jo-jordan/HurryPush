6aa57a0dc1a9b7a41784c9c94a100d709f3098ba
lzjlxebr <tyyzmyd@163.com>
2018-05-11 21:35:51 +0800

    add a parent class to two kind of fragments

---
+++ app/src/main/java/com/lzjlxebr/hurrypush/adapter/SurveyCardAbstractFragment.java
@@ -0,0 +1,25 @@
+package com.lzjlxebr.hurrypush.adapter;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public abstract class SurveyCardAbstractFragment extends Fragment {
+
+    public abstract View initView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState);
+
+    public abstract View getCardView();
+
+    public abstract void setCardTitleAndContent();
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return initView(inflater, container, savedInstanceState);
+    }
+}
