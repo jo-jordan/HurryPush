6691ecc00297b918216c3d12d99db81acc39d396
lzjlxebr <tyyzmyd@163.com>
2018-05-11 21:39:52 +0800

    update for add two kind of card view to pager view

--- app/src/main/java/com/lzjlxebr/hurrypush/adapter/SurveyCardFragmentAdapter.java
+++ app/src/main/java/com/lzjlxebr/hurrypush/adapter/SurveyCardFragmentAdapter.java
@@ -1,5 +1,6 @@
 package com.lzjlxebr.hurrypush.adapter;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
@@ -7,26 +8,40 @@ import android.support.v7.widget.CardView;
 import android.view.ViewGroup;
 
 import com.lzjlxebr.hurrypush.base.SurveyCardFragment;
+import com.lzjlxebr.hurrypush.base.SurveyFinalCardFragment;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class SurveyCardFragmentAdapter extends FragmentStatePagerAdapter implements CardAdapter {
 
-    private List<SurveyCardFragment> mFragments;
+    private List<SurveyCardAbstractFragment> mFragments;
     private float mBaseElevation;
 
-
-    public SurveyCardFragmentAdapter(FragmentManager fm, float baseElevation){
+    public SurveyCardFragmentAdapter(FragmentManager fm, float baseElevation) {
         super(fm);
 
         mFragments = new ArrayList<>();
         mBaseElevation = baseElevation;
 
-        for (int i = 0; i < 4; i++) {
-            addCardFragment(new SurveyCardFragment());
+        for (int i = 0; i < 3; i++) {
+            SurveyCardAbstractFragment fragment = new SurveyCardFragment();
+            Bundle bundle = new Bundle();
+            bundle.putString("card_fragment", "crad_fragment_" + i);
+            fragment.setArguments(bundle);
+
+            addCardFragment(fragment);
         }
+
+        SurveyCardAbstractFragment fragment = new SurveyFinalCardFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString("card_fragment", "crad_fragment_3");
+        fragment.setArguments(bundle);
+        //fm.beginTransaction().add(fragment,"card_fragment_3").commit();
+
+        addCardFragment(fragment);
     }
+
     @Override
     public Fragment getItem(int position) {
         return mFragments.get(position);
@@ -44,17 +59,17 @@ public class SurveyCardFragmentAdapter extends FragmentStatePagerAdapter impleme
 
     @Override
     public CardView getCardViewAt(int position) {
-        return mFragments.get(position).getCardView();
+        return (CardView) mFragments.get(position).getCardView();
     }
 
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
         Object fragment = super.instantiateItem(container, position);
-        mFragments.set(position,(SurveyCardFragment) fragment);
+        mFragments.set(position, (SurveyCardAbstractFragment) fragment);
         return fragment;
     }
 
-    public void addCardFragment(SurveyCardFragment surveyCardFragment){
+    public void addCardFragment(SurveyCardAbstractFragment surveyCardFragment) {
         mFragments.add(surveyCardFragment);
     }
 }
