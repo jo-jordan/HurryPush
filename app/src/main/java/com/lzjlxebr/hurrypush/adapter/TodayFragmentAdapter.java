2ff47e0075857dd5025d30963f9f2bb3e0c2918c
lzjlxebr <tyyzmyd@163.com>
2018-05-13 15:48:44 +0800

    add two type of view but not working

--- app/src/main/java/com/lzjlxebr/hurrypush/adapter/TodayFragmentAdapter.java
+++ app/src/main/java/com/lzjlxebr/hurrypush/adapter/TodayFragmentAdapter.java
@@ -10,9 +10,12 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.lzjlxebr.hurrypush.R;
-import com.lzjlxebr.hurrypush.ui.base.TodayFragment;
+import com.lzjlxebr.hurrypush.ui.base.MainActivity;
 
-public class TodayFragmentAdapter extends RecyclerView.Adapter<TodayFragmentAdapter.TodayAdapterViewHolder> {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class TodayFragmentAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private final Context mConetxt;
     private Cursor mCursor;
 
@@ -23,31 +26,71 @@ public class TodayFragmentAdapter extends RecyclerView.Adapter<TodayFragmentAdap
     @NonNull
     @Override
     public TodayAdapterViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
-        View view = LayoutInflater.from(mConetxt).inflate(R.layout.today_list_items, parent, false);
-
-        return new TodayAdapterViewHolder(view);
+        switch (viewType) {
+            case 0: {
+                View view = LayoutInflater.from(mConetxt).inflate(R.layout.today_list_items, parent, false);
+                return new TodayAdapterViewHolder(view);
+            }
+            case 2: {
+                View view = LayoutInflater.from(mConetxt).inflate(R.layout.today_list_items_survey, parent, false);
+                return new TodayAdapterViewHolder(view);
+            }
+            default:
+                View view = LayoutInflater.from(mConetxt).inflate(R.layout.today_list_items, parent, false);
+                return new TodayAdapterViewHolder(view);
+        }
     }
 
     @Override
-    public void onBindViewHolder(@NonNull TodayAdapterViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        switch (holder.getItemViewType()) {
+            case 0: {
+                TodayAdapterViewHolder todayHolder = (TodayAdapterViewHolder) holder;
+                mCursor.moveToPosition(position);
+
+                String appId = mCursor.getString(MainActivity.INDEX_COLUMN_APP_ID);
+                int gender = mCursor.getInt(MainActivity.INDEX_COLUMN_GENDER);
+                int currentLevelId = mCursor.getInt(MainActivity.INDEX_COLUMN_CURRENT_LEVEL_ID);
+                int CurrentLevelExp = mCursor.getInt(MainActivity.INDEX_COLUMN_CURRENT_EXP);
+                int upgradeExp = mCursor.getInt(MainActivity.INDEX_COLUMN_UPGRADE_EXP);
+                int isFirstTime = mCursor.getInt(MainActivity.INDEX_COLUMN_IS_FIRST_START);
+
+                todayHolder.todayTextViewContent.setText("APP_ID: " + appId + "\n");
+                todayHolder.todayTextViewContent.append("GENDER: " + gender + "\n");
+                todayHolder.todayTextViewContent.append("CURRENT LEVEL ID: " + currentLevelId + "\n");
+                todayHolder.todayTextViewContent.append("CURRENT LEVEL EXP: " + CurrentLevelExp + "\n");
+                todayHolder.todayTextViewContent.append("UPGRADE EXP: " + upgradeExp + "\n");
+                todayHolder.todayTextViewContent.append("IS_FIRST_START EXP: " + isFirstTime);
+
+                break;
+            }
+            case 2: {
+                TodayAdapterViewHolderSurvey todaySurveyHolder = (TodayAdapterViewHolderSurvey) holder;
+
+                mCursor.moveToPosition(position);
+
+                String appId = mCursor.getString(MainActivity.INDEX_COLUMN_APP_ID);
+                int gender = mCursor.getInt(MainActivity.INDEX_COLUMN_GENDER);
+                int currentLevelId = mCursor.getInt(MainActivity.INDEX_COLUMN_CURRENT_LEVEL_ID);
+                int CurrentLevelExp = mCursor.getInt(MainActivity.INDEX_COLUMN_CURRENT_EXP);
+                int upgradeExp = mCursor.getInt(MainActivity.INDEX_COLUMN_UPGRADE_EXP);
+                int isFirstTime = mCursor.getInt(MainActivity.INDEX_COLUMN_IS_FIRST_START);
+
+                todaySurveyHolder.todayTextViewSurveyContent.setText("APP_ID: " + appId + "\n");
+                todaySurveyHolder.todayTextViewSurveyContent.append("GENDER: " + gender + "\n");
+                todaySurveyHolder.todayTextViewSurveyContent.append("CURRENT LEVEL ID: " + currentLevelId + "\n");
+                todaySurveyHolder.todayTextViewSurveyContent.append("CURRENT LEVEL EXP: " + CurrentLevelExp + "\n");
+                todaySurveyHolder.todayTextViewSurveyContent.append("UPGRADE EXP: " + upgradeExp + "\n");
+                todaySurveyHolder.todayTextViewSurveyContent.append("IS_FIRST_START EXP: " + isFirstTime);
+
+                break;
+            }
+            default: {
+                return;
+            }
+        }
         mCursor.moveToPosition(position);
-
-        String appId = mCursor.getString(TodayFragment.INDEX_COLUMN_APP_ID);
-        int gender = mCursor.getInt(TodayFragment.INDEX_COLUMN_GENDER);
-        int currentLevelId = mCursor.getInt(TodayFragment.INDEX_COLUMN_CURRENT_LEVEL_ID);
-        int CurrentLevelExp = mCursor.getInt(TodayFragment.INDEX_COLUMN_CURRENT_EXP);
-        int upgradeExp = mCursor.getInt(TodayFragment.INDEX_COLUMN_UPGRADE_EXP);
-        int isFirstTime = mCursor.getInt(TodayFragment.INDEX_COLUMN_IS_FIRST_START);
-
-        holder.todayTextView.setText("APP_ID: " + appId + "\n");
-        holder.todayTextView.append("GENDER: " + gender + "\n");
-        holder.todayTextView.append("CURRENT LEVEL ID: " + currentLevelId + "\n");
-        holder.todayTextView.append("CURRENT LEVEL EXP: " + CurrentLevelExp + "\n");
-        holder.todayTextView.append("UPGRADE EXP: " + upgradeExp + "\n");
-        holder.todayTextView.append("IS_FIRST_START EXP: " + isFirstTime);
     }
-
 
     @Override
     public int getItemCount() {
@@ -60,13 +103,32 @@ public class TodayFragmentAdapter extends RecyclerView.Adapter<TodayFragmentAdap
         notifyDataSetChanged();
     }
 
+    @Override
+    public int getItemViewType(int position) {
+        return position == 0 ? 0 : 2;
+    }
+
     class TodayAdapterViewHolder extends RecyclerView.ViewHolder {
-        final TextView todayTextView;
+        @BindView(R.id.tv_today_title)
+        TextView todayTextViewTitle;
+        @BindView(R.id.tv_today_content)
+        TextView todayTextViewContent;
 
         TodayAdapterViewHolder(View view) {
             super(view);
+            ButterKnife.bind(this, view);
+        }
+    }
 
-            todayTextView = view.findViewById(R.id.today_tv);
+    class TodayAdapterViewHolderSurvey extends RecyclerView.ViewHolder {
+        @BindView(R.id.tv_today_survey_title)
+        TextView todayTextViewSurveyTitle;
+        @BindView(R.id.tv_today_survey_content)
+        TextView todayTextViewSurveyContent;
+
+        TodayAdapterViewHolderSurvey(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
         }
     }
 }
